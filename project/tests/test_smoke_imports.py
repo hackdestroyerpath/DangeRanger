603e86ce252180fbd7dccc4db6a4e8eea2f6b957
hackdestroyerpath <wisokyjaden@gmail.com>
2026-03-09 21:35:00 +0300

    Create test_smoke_imports.py

---
+++ project/tests/test_smoke_imports.py
@@ -0,0 +1,20 @@
+def test_import_main():
+    import main
+
+def test_import_main_func():
+    import main_func
+
+def test_import_logger():
+    import logger
+
+def test_import_settings():
+    import settings
+
+def test_import_wb():
+    import WB_parsing_func
+
+def test_import_ozon():
+    import OZON_parsing_func
+
+def test_import_combined():
+    import COMBINED_OUTPUR_FUNC
